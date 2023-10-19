3ddc92bedb8fb6b04629b1de9b19ad6ce70a7804
SruthiSivasankar <sivasankar.s@northeastern.edu>
2023-10-19 19:15:30 -0400

    Pulumi edit

--- index.js
+++ index.js
@@ -17,27 +17,26 @@ const awsDevProvider = new aws.Provider("awsdev", {
 });
 
 // VPC
-const vpc = new aws.ec2.Vpc("vpc", {
+const vpc = new aws.ec2.Vpc("myVpc", {
   cidrBlock: awsVpcCidr,
   enableDnsSupport: true,
   enableDnsHostnames: true,
   tags: {
-    Name: "VPC",
+    Name: "MyVPC",
   },
 });
 
 // Function to get the availability zones
 async function getAzs() {
   const zones = await aws.getAvailabilityZones({ state: "available" });
-  return zones.names.slice(0, desiredAzCount);
+  return zones.names.slice(0, 3);
 }
 
-const desiredAzCount = 3;
 const azs = pulumi.output(getAzs());
 
 // Create 3 public subnets and 3 private subnets, each in a different AZ
-const publicSubnets = azs.apply((azs) =>
-  azs.map((az, i) => {
+const publicSubnets = azs.apply((azNames) =>
+  azNames.map((az, i) => {
     return new aws.ec2.Subnet(`public-subnet-${i + 1}`, {
       vpcId: vpc.id,
       cidrBlock: `10.0.${i + 1}.0/24`,
@@ -65,20 +64,19 @@ const privateSubnets = azs.apply((azNames) =>
 );
 
 
+// Create an Internet Gateway and attach it to the VPC
+const internetGateway = new aws.ec2.InternetGateway("myInternetGateway", {
+  vpcId: vpc.id,
+  tags: {
+    Name: "MyInternetGateway",
+  },
+});
+
 // Create a public route table and associate public subnets
 const publicRouteTable = new aws.ec2.RouteTable("public-route-table", {
   vpcId: vpc.id,
   tags: {
     Name: "Public Route Table",
-  },
-});
-
-
-// Create a private route table and associate private subnets
-const privateRouteTable = new aws.ec2.RouteTable("private-route-table", {
-  vpcId: vpc.id,
-  tags: {
-    Name: "Private Route Table",
   },
 });
 
@@ -91,6 +89,14 @@ const publicSubnetAssociations = publicSubnets.apply((subnets) =>
   })
 );
 
+// Create a private route table and associate private subnets
+const privateRouteTable = new aws.ec2.RouteTable("private-route-table", {
+  vpcId: vpc.id,
+  tags: {
+    Name: "Private Route Table",
+  },
+});
+
 const privateSubnetAssociations = privateSubnets.apply((subnets) =>
   subnets.map((subnet, i) => {
     return new aws.ec2.RouteTableAssociation(`private-route-table-association-${i}`, {
@@ -100,13 +106,14 @@ const privateSubnetAssociations = privateSubnets.apply((subnets) =>
   })
 );
 
-
-const internetGateway = new aws.ec2.InternetGateway("internet-gateway", {
-  vpcId: vpc.id,
-  tags: {
-    Name: "main-gateway",
-  },
+// Create a public route in the public route table to the Internet Gateway
+const publicRoute = new aws.ec2.Route("public-route", {
+  routeTableId: publicRouteTable.id,
+  destinationCidrBlock: "0.0.0.0/0",
+  gatewayId: internetGateway.id,
 });
+
+
 
 
 new aws.ec2.Route("internet-route", {
@@ -116,6 +123,7 @@ new aws.ec2.Route("internet-route", {
 });
 
 
+// Example Application Security Group (customize as needed)
 const appSecurityGroup = new aws.ec2.SecurityGroup("appSecurityGroup", {
   vpcId: vpc.id,
   description: "Application Security Group",
@@ -156,28 +164,20 @@ new aws.ec2.SecurityGroupRule("appPortIngress", {
   type: "ingress",
   securityGroupId: appSecurityGroup.id,
   protocol: "tcp",
-  fromPort: 8080,  
-  toPort: 8080,    
+  fromPort: 6969,  
+  toPort: 6969,    
   cidrBlocks: ["0.0.0.0/0"],
 });
 
 
-const ec2Instance = new aws.ec2.Instance("Instance", {
+// EC2 Instance (customize instance details)
+const ec2Instance = new aws.ec2.Instance("webAppInstance", {
   ami: amiId,
   instanceType: "t2.micro",
   vpcSecurityGroupIds: [appSecurityGroup.id],
   keyName: keyName,
   subnetId: publicSubnets[0].id, 
   associatePublicIpAddress: true,
-  userData: `
-    #!/bin/bash
-
-    # Create and populate the .env file
-    echo 'DATABASE_URL=mysql://root:Sqlsru@19@127.0.0.1' > /path/to/your/project/directory/.env
-    echo 'DB_USERNAME=root' >> /path/to/your/project/directory/.env
-    echo 'DB_PASSWORD=Sqlsru@19' >> /path/to/your/project/directory/.env
-
-    `,
   rootBlockDevice: {
     volumeSize: 25,
     volumeType: "gp2",
@@ -187,5 +187,5 @@ const ec2Instance = new aws.ec2.Instance("Instance", {
     Name: "Webapp instance",
   },
   disableApiTermination: false,
-});
+});  
 
